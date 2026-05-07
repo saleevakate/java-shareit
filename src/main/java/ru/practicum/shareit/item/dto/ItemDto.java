736e1ee5889aed4ef0d103c7587109408b2c8c33
gabudakate <saleevakate@yandex.ru>
2026-05-07 18:50:08 +0500

    Реализованы Dto классы, первичная структура

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,13 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor  // нужен для создания объекта с полями
 public class ItemDto {
+    private String name;
+    private String description;
+    private Boolean available;
+    private Integer requestId;  // только ID запроса, не весь объект
 }
